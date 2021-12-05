4dc5ca4cf0177b0492f9c53a0eb8c92d92fdc52f
Jeremy Stanley <jeremy@instructure.com>
2021-12-04 22:24:17 -0700

    spaceship refactor

--- 5.rb
+++ 5.rb
@@ -15,39 +15,17 @@ def plot_point(points, x, y)
   points[[x, y]] += 1
 end
 
-def draw_vline(points, x, y0, y1)
-  (y0..y1).each do |y|
-    plot_point(points, x, y)
-  end
-end
-
-def draw_hline(points, y, x0, x1)
-  (x0..x1).each do |x|
-    plot_point(points, x, y)
-  end
-end
-
-def draw_diagonal(points, line)
-  dx = (line[1][0] > line[0][0]) ? 1 : -1
-  dy = (line[1][1] > line[0][1]) ? 1 : -1
+lines.each do |line|
+  dx = line[1][0] <=> line[0][0]
+  dy = line[1][1] <=> line[0][1]
   x = line[0][0]
   y = line[0][1]
   loop do
     plot_point(points, x, y)
-    break if x == line[1][0]
+    break if x == line[1][0] && y == line[1][1]
     x += dx
     y += dy
   end
 end
 
-lines.each do |line|
-  if line[0][0] == line[1][0]
-    draw_vline(points, line[0][0], *([line[0][1], line[1][1]].sort))
-  elsif line[0][1] == line[1][1]
-    draw_hline(points, line[0][1], *([line[0][0], line[1][0]].sort))
-  else
-    draw_diagonal(points, line)
-  end
-end
-
-puts points.values.select { |v| v > 1 }.count
+puts points.values.count { |v| v > 1 }
